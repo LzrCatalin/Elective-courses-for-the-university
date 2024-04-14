af79c4df4f36f68d96dc96d8313554875a940ca9
Robert <robert.ilea03@e-uvt.ro>
2024-04-14 14:51:10 +0300

    Added Application Service

---
+++ SpringProject/src/main/java/org/example/springproject/Services/ApplicationService.java
@@ -0,0 +1,9 @@
+package org.example.springproject.Services;
+
+import org.example.springproject.Entity.Application;
+
+import java.util.List;
+
+public interface ApplicationService {
+    public List<Application> getAllApplications();
+}
