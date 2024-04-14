6c21697612aa808591309dde5d3b111b704fb155
Robert <robert.ilea03@e-uvt.ro>
2024-04-14 14:41:52 +0300

    Added test method for an application

--- SpringProject/src/test/java/org/example/springproject/SpringProjectApplicationTests.java
+++ SpringProject/src/test/java/org/example/springproject/SpringProjectApplicationTests.java
@@ -1,13 +1,51 @@
 package org.example.springproject;
 
+import org.example.springproject.Entity.Application;
+import org.example.springproject.Entity.Course;
+import org.example.springproject.Entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class SpringProjectApplicationTests {
-
     @Test
     void contextLoads() {
     }
 
+    /**
+     * Test for adding a user to the system
+     */
+    @Test
+    void addUser() {
+        User user = new User(1L, "Dorel Marinescu", "student");
+        assertEquals("Dorel Marinescu", user.getName());
+        assertEquals("student", user.getRole());
+    }
+
+    /**
+     * Test for create an application to a course
+     */
+    @Test
+    void testApplication() {
+        User user = new User(1L, "Dorel Marinescu", "student");
+        assertEquals("Dorel Marinescu", user.getName());
+        assertEquals("student", user.getRole());
+
+        Course course = new Course("Math", "Geometry", 5, "John Doe", 30, "Mathematics");
+        assertEquals("Math", course.getName());
+        assertEquals("Geometry", course.getCategory());
+        assertEquals(5, course.getStudyYear());
+        assertEquals("John Doe", course.getTeacher());
+        assertEquals(30, course.getMaxCapacity());
+        assertEquals("Mathematics", course.getFacultySection());
+
+        Application application = new Application(user, course, 1, "pending");
+        assertEquals("Dorel Marinescu", application.getUser().getName());
+        assertEquals("Math", application.getCourse().getName());
+        assertEquals(1, application.getPriority());
+        assertEquals("pending", application.getStatus());
+
+    }
 }
