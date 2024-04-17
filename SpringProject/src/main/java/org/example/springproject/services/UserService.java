1119fcd06043bfa0c6268d7af06b94aa8a9ca6d0
Catalin Lazar <lazar.florin.catalin02@gmail.com>
2024-04-17 21:47:49 +0300

    Added modifications

--- SpringProject/src/main/java/org/example/springproject/services/UserService.java
+++ SpringProject/src/main/java/org/example/springproject/services/UserService.java
@@ -1,9 +1,9 @@
-package org.example.springproject.services;
-
-import org.example.springproject.entity.User;
-
-import java.util.List;
-
-public interface UserService {
-	public List<User> getAllUsers();
-}
+//package org.example.springproject.services;
+//
+//import org.example.springproject.entity.User;
+//
+//import java.util.List;
+//
+//public interface UserService {
+//	public List<User> getAllUsers();
+//}
