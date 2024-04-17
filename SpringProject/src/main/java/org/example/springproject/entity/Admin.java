2d0510ed7af45a204b4e3f7f5efe49505aa6fe25
Catalin Lazar <lazar.florin.catalin02@gmail.com>
2024-04-17 21:40:28 +0300

    Initial commit for Admin class

---
+++ SpringProject/src/main/java/org/example/springproject/entity/Admin.java
@@ -0,0 +1,12 @@
+package org.example.springproject.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "admins")
+public class Admin extends User{
+	/*
+	Empty class
+	 */
+}
