42002e0eff5e2c673a0fe8a316a8a570988456b4
Catalin Lazar <lazar.florin.catalin02@gmail.com>
2024-04-20 17:40:36 +0300

    Added Admin class implementations

--- SpringProject/src/main/java/org/example/springproject/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-//package org.example.springproject.repository;
-//
-//import org.example.springproject.entity.User;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface UserRepository extends JpaRepository<User, Long> {
-//	User findByName(String name);
-//}
