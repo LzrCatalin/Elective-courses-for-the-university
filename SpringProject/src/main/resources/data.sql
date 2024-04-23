482a113fcd4d1fc57541666e78a806f8681ea2d0
Catalin Lazar <lazar.florin.catalin02@gmail.com>
2024-04-23 17:14:14 +0300

    Data.sql file example

--- SpringProject/src/main/resources/data.sql
+++ SpringProject/src/main/resources/data.sql
@@ -1,13 +1,28 @@
-insert into users(name, role) values ('ion', 'admin');
-insert into users(name, role) values ('gheorghe', 'student');
+insert into students(grade, studyyear, facultysection, name, role) VALUES (8.76, 1, 0, 'John', 'student');
+insert into students(grade, studyyear, facultysection, name, role) VALUES (1, 1, 0, 'Jane', 'student');
+insert into students(grade, studyyear, facultysection, name, role) VALUES (1, 1, 1, 'Jack', 'student');
+insert into students(grade, studyyear, facultysection, name, role) VALUES (1, 1, 1, 'Jill', 'student');
+insert into students(grade, studyyear, facultysection, name, role) VALUES (1, 1, 1, 'James', 'student');
+insert into students(grade, studyyear, facultysection, name, role) VALUES (1, 1, 1, 'Jenny', 'student');
+insert into students(grade, studyyear, facultysection, name, role) VALUES (1, 1, 1, 'Jesse', 'student');
 
-insert into students(grade, studyyear, userid, facultysection) values (10, 1, 2, 'info');
+insert into admins(name) VALUES ('Ion');
 
-insert into courses(maxcapacity, studyyear, category, facultysection, name, teacher) values (10, 1, 'info', 'info', 'mate', 'ion');
+insert into courses(name, facultysection, studyyear, maxcapacity, teacher) VALUES ('Math', 0, 1, 2, 'Mr. Smith');
+insert into courses(name, facultysection, studyyear, maxcapacity, teacher) VALUES ('Physics', 0, 1, 2, 'Mr. Johnson');
+insert into courses(name, facultysection, studyyear, maxcapacity, teacher) VALUES ('Chemistry', 3, 1, 2, 'Mr. Brown');
+insert into courses(name, facultysection, studyyear, maxcapacity, teacher) VALUES ('Biology', 1, 1, 2, 'Mr. White');
+insert into courses(name, facultysection, studyyear, maxcapacity, teacher) VALUES ('History', 1, 1, 2, 'Mr. Black');
+insert into courses(name, facultysection, studyyear, maxcapacity, teacher) VALUES ('Geography', 1, 1, 2, 'Mr. Green');
 
-insert into application(priority, courseid, userid, status) VALUES (1, 1, 2, 'pending');
+insert into applications(studentid, courseid, status, priority) VALUES (1, 1, 2, 1);
+insert into applications(studentid, courseid, status, priority) VALUES (1, 2, 2, 2);
+insert into applications(studentid, courseid, status, priority) VALUES (2, 3, 2, 3);
+insert into applications(studentid, courseid, status, priority) VALUES (1, 1, 2, 4);
+insert into applications(studentid, courseid, status, priority) VALUES (1, 3, 2, 5);
 
-select * from users;
-select name, grade, studyyear, facultysection from students left join public.users u on u.id = students.userid;
-select * from application;
+select * from students;
+select * from admins;
+select * from courses;
+select * from applications;
 
