b6c7c4dcf00b5d9a3a350fea5134468740a84fff
Robert <robert.ilea03@e-uvt.ro>
2024-04-25 10:52:37 +0300

    Better approach

--- SpringProject/src/main/java/org/example/springproject/controller/CourseApi.java
+++ SpringProject/src/main/java/org/example/springproject/controller/CourseApi.java
@@ -4,6 +4,7 @@ import org.example.springproject.entity.Course;
 import org.example.springproject.enums.FacultySection;
 import org.example.springproject.services.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,16 +25,19 @@ public class CourseApi {
 
     @PostMapping("/")
     public ResponseEntity<String> addCourse(String name, String category, Integer studyYear, String teacher, Integer maxCapacity, FacultySection facultySection){
-        return courseService.addCourse(name,category,studyYear,teacher,maxCapacity,facultySection);
+        courseService.addCourse(name,category,studyYear,teacher,maxCapacity,facultySection);
+        return new ResponseEntity<>("Course added successfully!", HttpStatus.CREATED);
     }
-    @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteCourse(@PathVariable("id") Long id){
-        return courseService.deleteCourse(id);
-    }
+
     @PutMapping("/{id}")
     public ResponseEntity<String> updateCourse( @PathVariable("id") Long id,String name, String category, Integer studyYear, String teacher, Integer maxCapacity, FacultySection facultySection){
-        return courseService.updateCourse(id,name,category,studyYear,teacher,maxCapacity,facultySection);
+        courseService.updateCourse(id,name,category,studyYear,teacher,maxCapacity,facultySection);
+        return new ResponseEntity<>("Course with id: " + id + " successfully updated!", HttpStatus.OK);
     }
 
-
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteCourse(@PathVariable("id") Long id){
+        courseService.deleteCourse(id);
+        return new ResponseEntity<>("Course with id: " + id + " successfully deleted!", HttpStatus.OK);
+    }
 }
