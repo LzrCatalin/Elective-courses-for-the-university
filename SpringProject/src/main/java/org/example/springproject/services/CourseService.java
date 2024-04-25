1b521b021f175763bae9e23fdee3bd761bb519a5
Robert <robert.ilea03@e-uvt.ro>
2024-04-25 10:53:23 +0300

    Modified methods

--- SpringProject/src/main/java/org/example/springproject/services/CourseService.java
+++ SpringProject/src/main/java/org/example/springproject/services/CourseService.java
@@ -1,6 +1,5 @@
 package org.example.springproject.services;
 
-import com.zaxxer.hikari.util.ClockSource;
 import org.example.springproject.entity.Course;
 import org.example.springproject.enums.FacultySection;
 import org.springframework.http.ResponseEntity;
@@ -10,9 +9,9 @@ import java.util.List;
 public interface CourseService {
     public List<Course> getAllCourses();
 
-    ResponseEntity<String> addCourse(String name, String category, Integer studyYear, String teacher, Integer maxCapacity, FacultySection facultySection);
+    Course addCourse(String name, String category, Integer studyYear, String teacher, Integer maxCapacity, FacultySection facultySection);
 
-    ResponseEntity<String> deleteCourse(Long id);
+    void deleteCourse(Long id);
 
-    ResponseEntity<String> updateCourse(Long id, String name, String category, Integer studyYear, String teacher, Integer maxCapacity, FacultySection facultySection);
+    Course updateCourse(Long id, String name, String category, Integer studyYear, String teacher, Integer maxCapacity, FacultySection facultySection);
 }
