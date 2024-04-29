fb94d6ef8946ab93b851df13b381b36bc9ecf140
Catalin Lazar <lazar.florin.catalin02@gmail.com>
2024-04-30 00:59:12 +0300

    Changes based on exception handling

--- SpringProject/src/main/java/org/example/springproject/controller/ApplicationApi.java
+++ SpringProject/src/main/java/org/example/springproject/controller/ApplicationApi.java
@@ -2,6 +2,10 @@ package org.example.springproject.controller;
 
 import org.example.springproject.entity.Application;
 import org.example.springproject.enums.Status;
+import org.example.springproject.exceptions.DuplicatePriorityException;
+import org.example.springproject.exceptions.MismatchedFacultySectionException;
+import org.example.springproject.exceptions.MismatchedIdTypeException;
+import org.example.springproject.exceptions.NoSuchObjectExistsException;
 import org.example.springproject.services.ApplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -22,20 +26,45 @@ public class ApplicationApi {
 		return applicationService.getAllApplications();
 	}
 	@PostMapping("/")
-	public ResponseEntity<String> addApplication(Long studentId, Long courseId, Integer priority, Status status) {
-		applicationService.addApplication(studentId, courseId, priority, status);
-		return new ResponseEntity<>("Application added successfully.", HttpStatus.CREATED);
+	public ResponseEntity<String> addApplication(@RequestParam Long studentId,
+												 @RequestParam Long courseId, Integer priority, Status status) {
+		try {
+			applicationService.addApplication(studentId, courseId, priority, status);
+			return new ResponseEntity<>("Application added successfully.", HttpStatus.CREATED);
+
+		} catch (NoSuchObjectExistsException e) {
+			return new ResponseEntity<>(e.getMessage(), e.getHttpStatus());
+
+		} catch (MismatchedFacultySectionException e) {
+			return new ResponseEntity<>(e.getMessage(), e.getHttpStatus());
+
+		} catch (MismatchedIdTypeException e) {
+			return new ResponseEntity<>(e.getMessage(), e.getHttpStatus());
+
+		} catch (DuplicatePriorityException e) {
+			return new ResponseEntity<>(e.getMessage(), e.getHttpStatus());
+		}
 	}
 
 	@PutMapping("/{id}")
 	public ResponseEntity<String> updateApplication(@PathVariable("id") Long id, Integer priority, Status status) {
-		applicationService.updateApplication(id, priority, status);
-		return new ResponseEntity<>("Application with id:" + id + " successfully updated.", HttpStatus.OK);
+		try {
+			applicationService.updateApplication(id, priority, status);
+			return new ResponseEntity<>("Application with id:" + id + " successfully updated.", HttpStatus.OK);
+
+		} catch (NoSuchObjectExistsException e) {
+			return new ResponseEntity<>(e.getMessage(), e.getHttpStatus());
+		}
 	}
 
 	@DeleteMapping("/{id}")
 	public ResponseEntity<String> deleteApplication(@PathVariable("id") Long id) {
-		applicationService.deleteApplication(id);
-		return new ResponseEntity<>("Application with id:" + id + " successfully deleted.", HttpStatus.OK);
+		try {
+			applicationService.deleteApplication(id);
+			return new ResponseEntity<>("Application with id:" + id + " successfully deleted.", HttpStatus.OK);
+
+		} catch (NoSuchObjectExistsException e) {
+			return new ResponseEntity<>(e.getMessage(), e.getHttpStatus());
+		}
 	}
 }
--- SpringProject/src/main/java/org/example/springproject/controller/CourseApi.java
+++ SpringProject/src/main/java/org/example/springproject/controller/CourseApi.java
@@ -2,6 +2,10 @@ package org.example.springproject.controller;
 
 import org.example.springproject.entity.Course;
 import org.example.springproject.enums.FacultySection;
+import org.example.springproject.exceptions.InvalidCapacityException;
+import org.example.springproject.exceptions.InvalidNameException;
+import org.example.springproject.exceptions.InvalidStudyYearException;
+import org.example.springproject.exceptions.NoSuchObjectExistsException;
 import org.example.springproject.services.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -24,20 +28,49 @@ public class CourseApi {
     }
 
     @PostMapping("/")
-    public ResponseEntity<String> addCourse(String name, String category, Integer studyYear, String teacher, Integer maxCapacity, FacultySection facultySection){
-        courseService.addCourse(name,category,studyYear,teacher,maxCapacity,facultySection);
-        return new ResponseEntity<>("Course added successfully!", HttpStatus.CREATED);
+    public ResponseEntity<String> addCourse(@RequestParam String name, String category, Integer studyYear,
+                                            @RequestParam String teacher,
+                                            @RequestParam Integer maxCapacity, FacultySection facultySection){
+        try {
+            courseService.addCourse(name, category, studyYear, teacher, maxCapacity, facultySection);
+            return new ResponseEntity<>("Course added successfully!", HttpStatus.CREATED);
+
+        } catch (InvalidNameException e) {
+            return new ResponseEntity<>(e.getMessage(), e.getHttpStatus());
+
+        } catch (InvalidCapacityException e) {
+            return new ResponseEntity<>(e.getMessage(), e.getHttpStatus());
+
+        } catch (InvalidStudyYearException e) {
+            return new ResponseEntity<>(e.getMessage(), e.getHttpStatus());
+        }
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<String> updateCourse( @PathVariable("id") Long id,String name, String category, Integer studyYear, String teacher, Integer maxCapacity, FacultySection facultySection){
-        courseService.updateCourse(id,name,category,studyYear,teacher,maxCapacity,facultySection);
-        return new ResponseEntity<>("Course with id: " + id + " successfully updated!", HttpStatus.OK);
+    public ResponseEntity<String> updateCourse(@PathVariable("id") Long id,
+                                               @RequestParam String name, String category,
+                                               @RequestParam Integer studyYear,
+                                               @RequestParam String teacher, Integer maxCapacity, FacultySection facultySection){
+        try {
+            courseService.updateCourse(id, name, category, studyYear, teacher, maxCapacity, facultySection);
+            return new ResponseEntity<>("Course with id: " + id + " successfully updated!", HttpStatus.OK);
+
+        } catch (NoSuchObjectExistsException e) {
+            return new ResponseEntity<>(e.getMessage(), e.getHttpStatus());
+
+        } catch (InvalidNameException e) {
+            return new ResponseEntity<>(e.getMessage(), e.getHttpStatus());
+        }
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteCourse(@PathVariable("id") Long id){
-        courseService.deleteCourse(id);
-        return new ResponseEntity<>("Course with id: " + id + " successfully deleted!", HttpStatus.OK);
+        try {
+            courseService.deleteCourse(id);
+            return new ResponseEntity<>("Course with id: " + id + " successfully deleted!", HttpStatus.OK);
+
+        } catch (NoSuchObjectExistsException e) {
+            return new ResponseEntity<>(e.getMessage(), e.getHttpStatus());
+        }
     }
 }
