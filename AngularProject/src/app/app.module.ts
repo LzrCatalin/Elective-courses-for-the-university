2750a1fce0beeb0a7fb7d0f09ca73bea7cf93ae8
Gamidov Djahid <djahid.gamidov03@e-uvt.ro>
2024-05-03 17:44:21 +0300

    courses page

--- AngularProject/src/app/app.module.ts
+++ AngularProject/src/app/app.module.ts
@@ -5,15 +5,21 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { CourseComponent } from './components/course/course.component';
 import { ToolbarModule } from 'primeng/toolbar';
 import { ButtonModule } from 'primeng/button';
 import { RouterModule } from '@angular/router';
+import { TableModule } from 'primeng/table';
+import { InputTextModule } from 'primeng/inputtext';
+import { CascadeSelectModule } from 'primeng/cascadeselect';
+import { DragDropModule } from 'primeng/dragdrop';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CourseComponent
   ],
   imports: [
     BrowserModule,
@@ -21,8 +27,11 @@ import { RouterModule } from '@angular/router';
     BrowserAnimationsModule,
     ToolbarModule,
     ButtonModule,
-    RouterModule
-
+    RouterModule,
+    TableModule,
+    InputTextModule,
+    CascadeSelectModule,
+    DragDropModule,
   ],
   providers: [
     provideClientHydration()
