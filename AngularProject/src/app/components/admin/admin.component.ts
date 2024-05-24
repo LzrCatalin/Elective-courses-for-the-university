361304596dd5145ce682fb8d3fcc0e53e1b560eb
Robert <robert.ilea03@e-uvt.ro>
2024-05-24 21:15:06 +0300

    Replaced two containers, better layout for the containers

--- AngularProject/src/app/components/admin/admin.component.ts
+++ AngularProject/src/app/components/admin/admin.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { ReadOnlyService } from '../../services/read-only.service';
+import { StudentService } from '../../services/student.service';
+import { CourseService } from '../../services/course.service';
 
 @Component({
   selector: 'app-admin',
@@ -8,14 +10,61 @@ import { ReadOnlyService } from '../../services/read-only.service';
   styleUrl: './admin.component.css'
 })
 export class AdminComponent {
+	data: any;
+	options: any;
+	chartData: any;
+	chartOptions: any;
 
-	constructor(public readOnlyService: ReadOnlyService) {}
+	constructor(
+		public readOnlyService: ReadOnlyService,
+		private studentService: StudentService,
+		private courseService: CourseService) {}
 
-	toggleReadOnly() {
-		console.log("Toggle Read-Only...")
-		this.readOnlyService.readOnly$.subscribe(currentState => {
-		  this.readOnlyService.setReadOnly(!currentState);
+	ngOnInit() {
+		this.studentService.getFirstYearStudents().subscribe((res :any) => {
+			this.studentService.getSecondYearStudents().subscribe((res2 :any) => {
+				this.studentService.getThirdYearStudents().subscribe((res3 :any) => {
+					this.data = {
+						labels: ['First Year', 'Second Year	', 'Third Year'],
+						datasets: [
+							{
+								label: 'Students',
+								backgroundColor: ['#42A5F5', '#66BB6A', '#FFA726'],
+								borderColor: '#BLACK',
+								data: [res, res2, res3]
+							}
+						]
+					};
+				});
+			},
+			err => {
+				console.log(err);
+			});
+		},
+		err => {
+			console.log(err);
 		});
-	  }
+		this.options = {
+			legend: {
+			labels: {
+				fontColor: '#495057'
+			}
+			},
+			scales: {
+			xAxes: [{
+				ticks: {
+				fontColor: '#495057'
+				}
+			}],
+			yAxes: [{
+				ticks: {
+				fontColor: '#495057'
+				}
+			}]
+			}
+		};
+		
+	}
+	
 
 }
