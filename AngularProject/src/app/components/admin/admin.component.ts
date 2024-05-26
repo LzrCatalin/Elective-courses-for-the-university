39f20319101d379b650755eac574f21638416068
Robert <robert.ilea03@e-uvt.ro>
2024-05-26 15:51:10 +0300

    Design for admin interface

--- AngularProject/src/app/components/admin/admin.component.ts
+++ AngularProject/src/app/components/admin/admin.component.ts
@@ -3,68 +3,102 @@ import { BehaviorSubject } from 'rxjs';
 import { ReadOnlyService } from '../../services/read-only.service';
 import { StudentService } from '../../services/student.service';
 import { CourseService } from '../../services/course.service';
+import { MeterItem } from 'primeng/metergroup';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
-export class AdminComponent {
+
+export class AdminComponent{
 	data: any;
 	options: any;
 	chartData: any;
 	chartOptions: any;
+	value: MeterItem[]|undefined;
+	size: any;
 
 	constructor(
 		public readOnlyService: ReadOnlyService,
 		private studentService: StudentService,
 		private courseService: CourseService) {}
 
-	ngOnInit() {
-		this.studentService.getFirstYearStudents().subscribe((res :any) => {
-			this.studentService.getSecondYearStudents().subscribe((res2 :any) => {
-				this.studentService.getThirdYearStudents().subscribe((res3 :any) => {
-					this.data = {
-						labels: ['First Year', 'Second Year	', 'Third Year'],
-						datasets: [
+		ngOnInit() {
+			this.studentService.getFirstYearStudents().subscribe(
+			  (firstYearRes: any) => {
+				this.studentService.getSecondYearStudents().subscribe(
+				  (secondYearRes: any) => {
+					this.studentService.getThirdYearStudents().subscribe(
+					  (thirdYearRes: any) => {
+						this.data = {
+						  labels: ['Year'],
+						  datasets: [
 							{
-								label: 'Students',
-								backgroundColor: ['#42A5F5', '#66BB6A', '#FFA726'],
-								borderColor: '#BLACK',
-								data: [res, res2, res3]
+							  label: 'First Year',
+							  backgroundColor: '#988686',
+							  borderColor: '#000000',
+							  data: [firstYearRes]
+							},
+							{
+							  label: 'Second Year',
+							  backgroundColor: '#C8B4D0',
+							  borderColor: '#000000',
+							  data: [secondYearRes]
+							},
+							{
+							  label: 'Third Year',
+							  backgroundColor: '#5c4e4e',
+							  borderColor: '#000000',
+							  data: [thirdYearRes]
 							}
-						]
-					};
-				});
-			},
-			err => {
-				console.log(err);
-			});
-		},
-		err => {
-			console.log(err);
-		});
-		this.options = {
-			legend: {
-			labels: {
-				fontColor: '#495057'
-			}
-			},
-			scales: {
-			xAxes: [{
-				ticks: {
-				fontColor: '#495057'
-				}
-			}],
-			yAxes: [{
-				ticks: {
-				fontColor: '#495057'
-				}
-			}]
-			}
-		};
+						  ]
+						};
+					  },
+					  err => {
+						console.log('Error fetching third year students:', err);
+					  }
+					);
+				  },
+				  err => {
+					console.log('Error fetching second year students:', err);
+				  }
+				);
+			  },
+			  err => {
+				console.log('Error fetching first year students:', err);
+			  }
+			);
 		
-	}
-	
+			this.options = {
+			  legend: {
+				labels: {
+				  fontColor: '#495057'
+				}
+			  },
+			  scales: {
+				xAxes: [
+				  {
+					ticks: {
+					  fontColor: '#495057'
+					}
+				  }
+				],
+				yAxes: [
+				  {
+					ticks: {
+					  fontColor: '#495057'
+					}
+				  }
+				]
+			  }
+			};
+
+			this.value = [
+				{ label: 'Students', value: 40, color: '#42A5F5', icon: 'pi pi-user',},
+				{ label: 'Courses', value: 30, color: '#FFA726', icon: 'pi pi-box' },
+				{ label: 'Read-Only', color: '#AB47BC', icon: 'pi pi-exclamation-triangle' },
+			];
+		  }
 
 }
