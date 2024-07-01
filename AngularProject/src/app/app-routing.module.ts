88ffe9f16da11da57db5c7bfbd26c144b43cf12d
Catalin Lazar <lazar.florin.catalin02@gmail.com>
2024-07-01 23:42:24 +0300

    Added canActivate implementation

--- AngularProject/src/app/app-routing.module.ts
+++ AngularProject/src/app/app-routing.module.ts
@@ -11,22 +11,23 @@ import { MyprofileComponent } from './components/student-profile/myprofile.compo
 import { PostScheduleComponent } from './components/admin-add-schedule/post-schedule.component';
 import { GetSchedulesComponent } from './components/admin-view-schedules/get-schedules.component';
 import { LoginComponent } from './components/login/login.component';
+import { AuthService } from './auth/auth.service'
+import { AuthGuard } from './auth/auth.guard'
 
 const routes: Routes = [
 
   {path: '', component : LoginComponent},
-  {path : 'course', component : CourseComponent},
-  {path : 'admin', component: AdminComponent},
-  {path : 'admin/courses', component: GetCoursesComponent},
-  {path : 'admin/courses/post', component: PostCourseComponent},
-  {path : 'admin/schedule', component: GetSchedulesComponent},
-  {path : 'admin/schedule/post', component : PostScheduleComponent},
-  {path : 'admin/students', component: GetStudentsComponent},
-  {path : 'home/:studentId', component : HomeComponent},
-  {path : 'student/applications', component : GetApplicationsComponent},
-  {path : 'student/:studentId/applications', component : GetApplicationsComponent}, 
-  {path : 'student/:studentId/myprofile', component : MyprofileComponent},
-  {path : 'student/:studentId/courses', component : CourseComponent},
+  {path : 'course', component : CourseComponent, canActivate:[AuthGuard]},
+  {path : 'admin', component: AdminComponent, canActivate:[AuthGuard]},
+  {path : 'admin/courses', component: GetCoursesComponent, canActivate:[AuthGuard]},
+  {path : 'admin/courses/post', component: PostCourseComponent, canActivate:[AuthGuard]},
+  {path : 'admin/schedule', component: GetSchedulesComponent, canActivate:[AuthGuard]},
+  {path : 'admin/schedule/post', component : PostScheduleComponent, canActivate:[AuthGuard]},
+  {path : 'admin/students', component: GetStudentsComponent, canActivate:[AuthGuard]},
+  {path : 'home/:studentId', component : HomeComponent, canActivate:[AuthGuard]},
+  {path : 'student/:studentId/applications', component : GetApplicationsComponent, canActivate:[AuthGuard]}, 
+  {path : 'student/:studentId/myprofile', component : MyprofileComponent, canActivate:[AuthGuard]},
+  {path : 'student/:studentId/courses', component : CourseComponent, canActivate:[AuthGuard]},
   
 ];
 
